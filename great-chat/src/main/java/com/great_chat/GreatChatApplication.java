d08856a5dd66c62410d9e61684f0e084a77e51d4
luangf <luangf222@gmail.com>
2024-09-10 14:10:03 -0300

    Initial commit

---
+++ great-chat/src/main/java/com/great_chat/GreatChatApplication.java
@@ -0,0 +1,13 @@
+package com.great_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatChatApplication.class, args);
+	}
+
+}
---
+++ great-chat/src/test/java/com/great_chat/GreatChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.great_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
