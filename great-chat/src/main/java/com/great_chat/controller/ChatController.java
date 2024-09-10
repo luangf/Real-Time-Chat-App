4037f45c5a9eec9f5ed6a6dc20abf375bc9e050a
luangf <luangf222@gmail.com>
2024-09-10 16:24:10 -0300

    commit 2

--- great-chat/src/main/java/com/great_chat/controller/ChatController.java
+++ great-chat/src/main/java/com/great_chat/controller/ChatController.java
@@ -1,8 +1,27 @@
 package com.great_chat.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+
+import com.great_chat.general.ChatMessage;
 
 @Controller
 public class ChatController {
 
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/public") //which topic/queue
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+	
+	@MessageMapping("/chat.addUser")
+	@SendTo("/topic/public")
+	public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender()); //add username in websocket session
+		return chatMessage;
+	}
+	
 }
---
+++ great-chat/src/main/java/com/great_chat/general/MessageType.java
@@ -0,0 +1,9 @@
+package com.great_chat.general;
+
+public enum MessageType {
+
+	CHAT,
+	JOIN,
+	LEAVE
+	
+}
